a27c3af5dca100711c640de21107f8732c9ce39b
Yousef Qandeel <yqandil7@gmail.com>
2022-03-03 04:50:00 +0300

    not complete :(

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,12 +2,19 @@
 import Header from "../header/Header";
 import Tours from "../tours/Tours";
 import Footer from "../footer/Footer";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import TourDetails from "../TourDetails/TourDetails"
+
 function Home(props) {
     return (
         <>
             <Header />
-            <Tours tours={props.data} />
-            <Footer />
+            <Routes>
+                <Route path="/" element={<Tours data={props.data} />} />
+                <Route path="/:id" element={<TourDetails data={props.data} />} />
+            </Routes>
+            <Footer></Footer>
         </>
     )
 };
