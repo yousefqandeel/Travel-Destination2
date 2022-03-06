e65d63fedc82b0201e55bd6b82e0cafea121d1e3
Yousef Qandeel <yqandil7@gmail.com>
2022-03-06 04:29:24 +0300

    class 17

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Home from "./components/home/Home";
 import { Routes, Route } from "react-router-dom";
-
+import TourDetails from "./components/TourDetails/TourDetails";
 import data from "./data/data.json";
 
 function App() {
     return (
-        <>
-            <Home data={data} />
-        </>
+        <Routes>
+            <Route path="/" element={<Home data={data} />} />
+            <Route path="/city/:id" element={<TourDetails data={data} />} />
+        </Routes>
     )
 };
 export default App;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 import Header from "../header/Header";
 import Tours from "../tours/Tours";
 import Footer from "../footer/Footer";
